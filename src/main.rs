6aebe86e560bf10c3fcd70c5b156111690e13688
Mendess2526 <pedro.mendes.26@gmail.com>
2019-07-09 11:46:05 +0100

    Fix boiler plate

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
-use serenity::model::channel::Message;
-use serenity::model::gateway::Ready;
-use serenity::prelude::*;
+use serenity::{
+    framework::standard::{
+        help_commands, macros::help, Args, CommandGroup, CommandResult, HelpOptions,
+        StandardFramework,
+    },
+    model::{
+        channel::{Channel, Message},
+        gateway::Ready,
+        id::{GuildId, UserId},
+        voice::VoiceState,
+    },
+    prelude::*,
+};
 
 const TOKEN: &str = "TOKEN HERE";
 
 struct Handler;
 
-impl EventHandler for Handler {
-    fn message(&self, ctx: Context, msg: Message) {
-        if !msg.author.bot {
-            if let Err(why) = msg.channel_id.say(ctx, "Hello!") {
-                println!("Message Error: {:?}", why);
-            }
-        }
-    }
-
-    fn ready(&self, _: Context, _: Ready) {
-        println!("I'm ready boy!");
-    }
-}
+impl EventHandler for Handler {}
 
 fn main() {
     let mut client = Client::new(TOKEN, Handler).expect("Error creating client");
