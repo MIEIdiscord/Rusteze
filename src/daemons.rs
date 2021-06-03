75db096b2d0f0939d6e2ea6636a8cc1f20da9d5d
mendess <pedro.mendes.26@gmail.com>
2021-06-03 16:09:42 +0100

    Use monomorphise macro

--- src/daemons.rs
+++ src/daemons.rs
@@ -1,37 +1,11 @@
 pub mod minecraft;
 
 use serenity::{prelude::TypeMapKey, CacheAndHttp};
-use std::{ops::{Deref, DerefMut}, sync::Arc };
+use std::sync::Arc;
 use tokio::sync::Mutex;
 
-pub struct DaemonManager(daemons::DaemonManager<CacheAndHttp>);
-
-impl DaemonManager {
-    pub fn new(data: Arc<CacheAndHttp>) -> Self {
-        data.into()
-    }
-}
-
-impl From<Arc<CacheAndHttp>> for DaemonManager {
-    fn from(data: Arc<CacheAndHttp>) -> Self {
-        Self(data.into())
-    }
-}
+daemons::monomorphise!(CacheAndHttp);
 
 impl TypeMapKey for DaemonManager {
     type Value = Arc<Mutex<DaemonManager>>;
-}
-
-impl Deref for DaemonManager {
-    type Target = daemons::DaemonManager<CacheAndHttp>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl DerefMut for DaemonManager {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
 }
